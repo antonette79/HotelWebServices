9c11adc542de8831300e426c1a71f158dd2ab03d
Jane Steg <jane5185@edu.easj.dk>
2016-04-15 11:47:40 +0200

    Opret View

---
+++ WebClient/Model/GuestCatalogSingelton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WebClient.Model
+{
+    public class GuestCatalogSingelton
+    {
+        private static GuestCatalogSingelton _instance;
+
+        private GuestCatalogSingelton()
+        {
+            var guests = new ObservableCollection<Guest>();
+        }
+
+        public ObservableCollection<Guest> guests { get; set; }
+
+        public static GuestCatalogSingelton Instance
+        {
+            get { return _instance ?? (_instance = new GuestCatalogSingelton()); }
+            
+        }
+
+        public void Add(Guest newGuest)
+        {
+            guests.Add(newGuest);
+        }
+
+        public void Remove(Guest removeGuest)
+        {
+            guests.Remove(removeGuest);
+        }
+    }
+}
