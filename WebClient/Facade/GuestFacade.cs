8cfc543d6662cd8504d06b9db29b15e56617c803
Jane Steg <jane5185@edu.easj.dk>
2016-04-26 12:56:50 +0200

    Fået listviewet til at fungere, og update...

---
+++ WebClient/Facade/GuestFacade.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Newtonsoft.Json;
+using WebClient.Model;
+
+namespace WebClient.Facade
+{
+    class GuestFacade
+    {
+        public static Guest Guest { get; set; }
+        public static IEnumerable<Guest> Guests { get; set; }
+        public const string ServerUrl = "http://localhost:7908/";
+
+        public static async Task<IEnumerable<Guest>> GetGuestsAsync()
+        {
+            Guests = null;
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                UseDefaultCredentials = true
+            };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = await client.GetAsync("api/guests");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = response.Content.ReadAsStringAsync().Result;
+                        Guests = JsonConvert.DeserializeObject<IEnumerable<Guest>>(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog msgDialog = new MessageDialog(ex.Message, "Error");
+                    await msgDialog.ShowAsync();
+                    throw;
+                }
+                return Guests;
+            }
+        }
+        public static async Task<Guest> GetGuest(int id)
+        {
+            Guest = null;
+            HttpClientHandler handler = new HttpClientHandler
+            { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = await client.GetAsync("api/guests");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = response.Content.ReadAsStringAsync().Result;
+                        var listofGuests = JsonConvert.DeserializeObject<List<Guest>>(result);
+                        foreach (var item in listofGuests)
+                        {
+                            Guest = item.Guest_No == id ? item : null;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog msgDialog = new MessageDialog(ex.Message, "Error");
+                    await msgDialog.ShowAsync();
+                    throw;
+                }
+                return Guest;
+            }
+        }
+        public static async Task<string> PostGuest(Guest guest)
+        {
+            Guest = guest;
+            HttpClientHandler handler = new HttpClientHandler
+            { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = await client.PostAsJsonAsync("api/guests", Guest);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return "Guest Created";
+                    }
+                    return "Error Creating Guest" + response.StatusCode;
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog msgDialog = new MessageDialog(ex.Message, "Error");
+                    await msgDialog.ShowAsync();
+                    throw;
+                }
+            }
+        }
+        public static async Task<string> DeleteGuest(Guest guest)
+        {
+            Guest = guest;
+            HttpClientHandler handler = new HttpClientHandler {UseDefaultCredentials = true};
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = await client.DeleteAsync("api/guests/" + Guest.Guest_No);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return "Guest Deleted";
+                    }
+                    return "Error Deleting Guest: " + response.StatusCode;
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog msgDialog = new MessageDialog(ex.Message, "Error");
+                    await msgDialog.ShowAsync();
+                    throw;
+                }
+            }
+        }
+      public static async Task<string> PutGuestAsync(Guest guest, int id)
+        {
+            Guest = guest;
+            HttpClientHandler handler = new HttpClientHandler { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = await client.PutAsJsonAsync("api/guests/" + id, Guest);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return "Guest Updated";
+                    }
+                    return "Error Updating Guest: " + response.StatusCode;
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog msgDialog = new MessageDialog(ex.Message, "Runtime Error");
+                    await msgDialog.ShowAsync();
+                    throw;
+                }
+            }
+        }
+    }
+}
--- WebClient/ViewModel/ViewModel.cs
+++ WebClient/ViewModel/ViewModel.cs
@@ -5,26 +5,85 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Media.Animation;
 using WebClient.Annotations;
+using WebClient.Facade;
 using WebClient.Model;
 
 namespace WebClient.ViewModel
 {
     class ViewModel : INotifyPropertyChanged
     {
+        #region Propeties
         public GuestCatalogSingelton Guests { get; set; }
-        public int Guest_No { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+        private Guest _selectedGuest = new Guest();
+        public Guest SelectedGuest
+        {
+            get { return _selectedGuest; }
+            set
+            {
+                _selectedGuest = value;
+                RaisePropertyChanged("SelectedGuest");
+            }
+        }
+        #endregion
 
+        #region Constructors
         public ViewModel()
         {
             Guests = GuestCatalogSingelton.Instance;
-            Guests.Add(new Guest(1, "Flunky Foe", "Roskildevej 98, 4000 Roskilde"));
-            Guests.Add(new Guest(2, "Betta Boe", "Holbækvej 128, 4300 Holbæk"));
-            Guests.Add(new Guest(3, "Hola Hup", "Ringstedvej 13, 4300 Holbæk"));
-            Guests.Add(new Guest(4,"Skipping School", "Københavnsvej 456, 3400 Valby"));
+
+            //Guests.Add(new Guest(1, "Flunky Foe", "Roskildevej 98, 4000 Roskilde"));
+            //Guests.Add(new Guest(2, "Betta Boe", "Holbækvej 128, 4300 Holbæk"));
+            //Guests.Add(new Guest(3, "Hola Hup", "Ringstedvej 13, 4300 Holbæk"));
+            //Guests.Add(new Guest(4,"Skipping School", "Københavnsvej 456, 3400 Valby"));
+
+            GetGuests();
+        }
+        #endregion
+
+        public async void PutGuestAsync()
+        {
+            //var guest = new Guest(SelectedGuest.Guest_No, Name, Address);
+            var msg = await GuestFacade.PutGuestAsync(SelectedGuest, SelectedGuest.Guest_No);
+
+            MessageDialog message = new MessageDialog(msg);
+            await message.ShowAsync();
+            GetGuests();
+
+
+        }
+        public async void GetGuests()
+        {
+            Guests.Guests.Clear();
+
+            IEnumerable<Guest> guestListTemp = await Facade.GuestFacade.GetGuestsAsync();
+            foreach (var item in guestListTemp)
+            {
+                Guests.Add(item);
+            }
+        }
+
+        public void SelectGuest()
+        {
+            if (SelectedGuest != null)
+            {
+                Guest_No = SelectedGuest.Guest_No;
+                Name = SelectedGuest.Name;
+                Address = SelectedGuest.Address;
+
+            }
+            else
+            {
+                MessageDialog messageDialog = new MessageDialog("Fejl", "Kunne ikke sætte bruger til admin!!!");
+            }
+        }
+
+        public void PopulateList()
+        {
+            Guests.Guests.Clear();
+            GetGuests();
         }
 
         #region PropertyChangedSupport
@@ -34,6 +93,13 @@ namespace WebClient.ViewModel
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
 
         #endregion
